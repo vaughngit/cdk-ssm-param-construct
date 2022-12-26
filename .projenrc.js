bf3e4654cacd1c4c0dea1aad1febaaa65ef0c63b
VaughnGit <alvin@vaughns.net>
2022-12-26 00:49:09 -0600

    maint: projen file updated

--- .projenrc.js
+++ .projenrc.js
@@ -22,6 +22,12 @@ const project = new awscdk.AwsCdkConstructLibrary({
     'sdk',
   ],
   license: 'Apache-2.0',
+  autoApproveUpgrades: true,
+  // dependabot: true,
+  autoApproveOptions: {
+    allowedUsernames: ['github-bot', 'vaughngit', '*'],
+    //secret: 'PROJEN_GITHUB_WORKFLOWS',
+  },
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
@@ -36,7 +42,33 @@ const project = new awscdk.AwsCdkConstructLibrary({
     packageId: 'construct.ssm.parameter',
   },
 
+  gitpod: true,
 });
+
+project.gitpod.addCustomTask({
+  init: 'yarn install && yarn run build',
+  command: 'yarn run watch',
+});
+
+project.gitpod.addCustomTask({
+  name: 'ConfigAlias',
+  command: 'echo \'alias pj="npx projen"\' >> ~/.bashrc && echo \'alias cdk="npx cdk"\' >> ~/.bashrc',
+});
+
+project.gitpod.addCustomTask({
+  name: 'Initialize & Configure AWS',
+  command: 'bash $GITPOD_REPO_ROOT/configure_aws_with_gitpod.sh',
+});
+
+project.gitpod.addCustomTask({
+  name: 'Install DOTNET 6.0',
+  command: 'bash $GITPOD_REPO_ROOT/configure_dotnet.sh',
+});
+
+project.gitpod.addVscodeExtensions(
+  'ms-azuretools.vscode-docker',
+  'AmazonWebServices.aws-toolkit-vscode',
+);
 
 
 project.synth();
